aad3e7f66d91edd5ea0a6592a66cc56e008d5bcb
CoderBoy4715 <alijahlemay12@gmail.com>
2020-10-16 11:26:39 -0700

    still working on carrousel

--- frontend/src/components/utils/Carrousel/index.js
+++ frontend/src/components/utils/Carrousel/index.js
@@ -2,6 +2,35 @@ import React, { useState, useEffect, useRef } from 'react';
 import styled, { keyframes } from 'styled-components';
 
 const Carrousel = ({ images, size }) => {
+  const slideAnimation = keyframes`${(props) => props.values}`;
+
+  const CarouselImage = styled.img`
+    height: 100px;
+    width: 100px;
+    position: absolute;
+    top: 0;
+    left: ${(props) => props.startingPos};
+    background-color: ${(props) => props.color};
+    animation-name: ${(props) => props.values};
+    animation-duration: 8s;
+    animation-timing-function: linear;
+    animation-iteration-count: infinite;
+  `;
+
+  CarouselImage.defaultProps = {
+    startingPos: '0',
+  };
+
+  const getKeyframes = (index) =>
+    keyframes`${15 * (index + 1)}% {transform: translateX(${100 * index}px);} ${
+      15 * (index + 1) + 5
+    }% {transform: translateX(${100 * index}px);}`;
+
+  const getValues = (index) => {
+    return `${15 * (index + 1)}% {transform: translateX(${100 * index}px);} ${
+      15 * (index + 1) + 5
+    }% {transform: translateX(${100 * index}px);}`;
+  };
   return (
     <div
       style={{
@@ -11,39 +40,19 @@ const Carrousel = ({ images, size }) => {
         height: '100px',
       }}
     >
-      {images.map((image, index) => (
-        <CarouselImage
-          key={index}
-          src={image}
-          startingPos={`${100 * index}px`}
-        />
-      ))}
+      {images.map((image, index) => {
+        console.log(`values for image ${image}${index} = ${getValues(index)}`);
+        return (
+          <CarouselImage
+            key={index}
+            src={image}
+            startingPos={`${100 * index}px`}
+            values={getKeyframes(index)}
+          />
+        );
+      })}
     </div>
   );
 };
-const giveKeyValues = () => {
-  return ``;
-};
 
-const slideAnimation = keyframes`0% {transform: translateX(${(props) =>
-  props.startingPos});}
-15% {transform: translateX(calc(100px));}
-100% {transform: translateX(calc(100px));}`;
-
-const CarouselImage = styled.img`
-  height: 100px;
-  width: 100px;
-  position: absolute;
-  top: 0;
-  left: ${(props) => props.startingPos};
-  background-color: ${(props) => props.color};
-  animation-name: ${slideAnimation};
-  animation-duration: 8s;
-  animation-timing-function: linear;
-  animation-iteration-count: infinite;
-`;
-
-CarouselImage.defaultProps = {
-  startingPos: '0',
-};
 export default Carrousel;

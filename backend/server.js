4627293daf5f6e00cb0d760be7d0fa213fd5ddb0
CoderBoy4715 <alijahlemay12@gmail.com>
2020-11-03 11:47:41 -0800

    changed port #

--- backend/server.js
+++ backend/server.js
@@ -38,6 +38,6 @@ app.use('/uploads', express.static(path.join(dirname, '/uploads')));
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

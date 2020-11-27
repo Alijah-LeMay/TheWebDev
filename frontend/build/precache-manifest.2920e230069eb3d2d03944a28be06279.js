3bced61e83148107c6a8b6830287b3a9fc1caa2c
CoderBoy4715 <alijahlemay12@gmail.com>
2020-11-27 12:02:40 -0800

    added build- ready to redeploy

---
+++ frontend/build/precache-manifest.2920e230069eb3d2d03944a28be06279.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e96687f3da6020c1a5df88793c206d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40580aae634910a85537",
+    "url": "/static/css/main.4392d6ea.chunk.css"
+  },
+  {
+    "revision": "5cf52d3f7fd8300a091b",
+    "url": "/static/js/2.011319cd.chunk.js"
+  },
+  {
+    "revision": "f96e21c66be6e9d23656aeea86222d22",
+    "url": "/static/js/2.011319cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40580aae634910a85537",
+    "url": "/static/js/main.09fbeeae.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "33a58d6fcf54c80d5eff2b537d054ad9",
+    "url": "/static/media/banner_white.33a58d6f.png"
+  },
+  {
+    "revision": "891b553fe3b1055da7222784c2465fb3",
+    "url": "/static/media/blog_page.891b553f.jpg"
+  },
+  {
+    "revision": "45ddeafdddc61812f1f0566854584c2a",
+    "url": "/static/media/landing_bck.45ddeafd.jpg"
+  },
+  {
+    "revision": "1c59759fb6b6b27a3e02d1022c69670c",
+    "url": "/static/media/quote_bck.1c59759f.jpg"
+  }
+]);
